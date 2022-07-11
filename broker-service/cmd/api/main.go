1829ea8d23ba6840ff0aacf78068c5797ff88f4c
samgozman <sam@gozman.space>
2022-07-11 23:27:38 +0400

    Refactor the line

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -35,9 +35,8 @@ func main() {
 		return
 	}
 	defer loggerServiceConn.Close()
-	loggerClient := logs.NewLogServiceClient(loggerServiceConn)
 	logger := Logger{
-		client: loggerClient,
+		client: logs.NewLogServiceClient(loggerServiceConn),
 	}
 
 	// ! Move client connections to the separate gorutine
