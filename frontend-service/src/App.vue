171ff3553d5818c9548b6dc08f7223f72e23a661
samgozman <sam@gozman.space>
2022-07-14 22:00:16 +0400

    Add cleaner navbar

--- frontend-service/src/App.vue
+++ frontend-service/src/App.vue
@@ -12,18 +12,7 @@ import Footer from "./components/FooterComponent.vue";
       </div>
     </header>
     <div class="container xl:mx-auto mb-auto">
-      <div class="drawer drawer-end">
-        <input id="right-sidebar" type="checkbox" class="drawer-toggle" />
-        <div class="drawer-content p-6">
-          <RouterView />
-        </div>
-        <div class="drawer-side">
-          <label for="right-sidebar" class="drawer-overlay"></label>
-          <div class="p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-            Place for login / register form
-          </div>
-        </div>
-      </div>
+      <RouterView />
     </div>
     <Footer />
   </div>
--- frontend-service/src/components/NavBar.vue
+++ frontend-service/src/components/NavBar.vue
@@ -1,22 +1,35 @@
 <script setup lang="ts">
 import { RouterLink } from "vue-router";
+import BigButton from "./buttons/BigButton.vue";
 </script>
 
 <template>
   <div class="navbar bg-base-100">
     <div class="navbar-start">
       <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-    </div>
-    <div class="navbar-center">
       <ul class="menu menu-horizontal p-0">
         <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/dashboard">Dashboard</RouterLink></li>
         <li><RouterLink to="/about">About</RouterLink></li>
       </ul>
     </div>
+    <div class="navbar-center"></div>
     <div class="navbar-end">
-      <label for="right-sidebar" class="drawer-button btn btn-primary">
-        Get started
-      </label>
+      <BigButton msg="+ Add new" class="mr-3" />
+      <div class="dropdown dropdown-end">
+        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <img src="https://placeimg.com/80/80/people" />
+          </div>
+        </label>
+        <ul
+          tabindex="0"
+          class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+        >
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
