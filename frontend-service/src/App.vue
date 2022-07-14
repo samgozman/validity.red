acafadc5fbc3ef94daf1be45af59bb5838864e61
samgozman <sam@gozman.space>
2022-07-14 18:43:14 +0400

    Add sidebar

--- frontend-service/src/App.vue
+++ frontend-service/src/App.vue
@@ -12,7 +12,18 @@ import Footer from "./components/FooterComponent.vue";
       </div>
     </header>
     <div class="container xl:mx-auto mb-auto">
-      <RouterView />
+      <div class="drawer drawer-end">
+        <input id="right-sidebar" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content p-6">
+          <RouterView />
+        </div>
+        <div class="drawer-side">
+          <label for="right-sidebar" class="drawer-overlay"></label>
+          <div class="p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+            Place for login / register form
+          </div>
+        </div>
+      </div>
     </div>
     <Footer />
   </div>
--- frontend-service/src/components/NavBar.vue
+++ frontend-service/src/components/NavBar.vue
@@ -5,52 +5,18 @@ import { RouterLink } from "vue-router";
 <template>
   <div class="navbar bg-base-100">
     <div class="navbar-start">
-      <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
-        </label>
-        <ul
-          tabindex="0"
-          class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li><RouterLink to="/">Home</RouterLink></li>
-          <li><RouterLink to="/about">About</RouterLink></li>
-        </ul>
-      </div>
-    </div>
-    <div class="navbar-center">
       <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
     </div>
+    <div class="navbar-center">
+      <ul class="menu menu-horizontal p-0">
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+      </ul>
+    </div>
     <div class="navbar-end">
-      <button class="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
-      </button>
+      <label for="right-sidebar" class="drawer-button btn btn-primary">
+        Get started
+      </label>
     </div>
   </div>
 </template>
