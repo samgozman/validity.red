aca09f699dbf0ec5d3d749d18e21ccd5371cfb22
samgozman <sam@gozman.space>
2022-07-15 09:45:14 +0400

    Add hero block for main view

--- frontend-service/src/views/MainView.vue
+++ frontend-service/src/views/MainView.vue
@@ -4,10 +4,21 @@ import BigButton from "../components/buttons/BigButton.vue";
 
 <template>
   <main>
-    <article class="prose lg:prose-xl">
-      <h2>Main view</h2>
-      <p>Some text</p>
-    </article>
-    <BigButton msg="Some button" />
+    <div class="hero min-h-screen bg-base-100">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <article class="prose lg:prose-xl mb-10">
+            <h1>Main view</h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+              sunt aliquam amet quaerat provident possimus eius, ex quos odit
+              earum debitis molestias eligendi accusamus adipisci consequatur
+              reiciendis quo aliquid nemo?
+            </p>
+          </article>
+          <BigButton msg="Some button" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
