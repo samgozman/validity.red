8d675cb14a7b3ee29d593b5a260052cf57a3f72e
samgozman <sam@gozman.space>
2022-07-16 08:47:26 +0400

    Add custom themes

--- frontend-service/tailwind.config.js
+++ frontend-service/tailwind.config.js
@@ -5,5 +5,38 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        "validity-light": {
+          primary: "#e11d48",
+          secondary: "#fb7185",
+          accent: "#4b5563",
+          neutral: "#191D24",
+          "base-100": "#FFFFFF",
+          "base-200": "#EBF1F4",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+      {
+        "validity-dark": {
+          primary: "#e11d48",
+          secondary: "#fb7185",
+          accent: "#4b5563",
+          neutral: "#191D24",
+          "base-100": "#2e3b51",
+          "base-200": "#252e41",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+    darkTheme: "validity-dark",
+  },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
