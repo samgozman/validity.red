e8d48013e7fde30f7c3099af0945aff46a9f1d83
samgozman <sam@gozman.space>
2022-07-27 19:51:08 +0400

    Add VerifyPassword, BeforeCreate and BeforeSave methods

--- user-service/internal/models/user/User_test.go
+++ user-service/internal/models/user/User_test.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"testing"
+
+	"gorm.io/gorm"
 )
 
 func TestUser_Prepare(t *testing.T) {
@@ -110,6 +112,144 @@ func TestHash(t *testing.T) {
 			}
 			if len(got) == 0 && tt.wantErr {
 				t.Errorf("Hash is empty")
+			}
+		})
+	}
+}
+
+func TestVerifyPassword(t *testing.T) {
+	type args struct {
+		hashedPassword string
+		password       string
+	}
+
+	testPass := "foopassword"
+	hashedPass, _ := Hash(testPass)
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "should pass",
+			args:    args{hashedPassword: string(hashedPass), password: testPass},
+			wantErr: false,
+		},
+		{
+			name:    "should fail",
+			args:    args{hashedPassword: string(hashedPass), password: "wrongpassword"},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := VerifyPassword(tt.args.hashedPassword, tt.args.password); (err != nil) != tt.wantErr {
+				t.Errorf("VerifyPassword() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestUser_BeforeCreate(t *testing.T) {
+	type args struct {
+		tx *gorm.DB
+	}
+
+	a := args{
+		tx: &gorm.DB{},
+	}
+
+	tests := []struct {
+		name    string
+		user    User
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "should pass",
+			user:    User{Email: "test@example.com", Password: "password"},
+			args:    a,
+			wantErr: false,
+		},
+		{
+			name:    "should fail on empty password",
+			user:    User{Email: "test@example.com", Password: ""},
+			args:    a,
+			wantErr: true,
+		},
+		{
+			name:    "should fail on empty email",
+			user:    User{Email: "", Password: "password"},
+			args:    a,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &User{
+				ID:         tt.user.ID,
+				Email:      tt.user.Email,
+				Password:   tt.user.Password,
+				IsVerified: tt.user.IsVerified,
+				CreatedAt:  tt.user.CreatedAt,
+				UpdatedAt:  tt.user.UpdatedAt,
+			}
+			if err := u.BeforeCreate(tt.args.tx); (err != nil) != tt.wantErr {
+				t.Errorf("User.BeforeCreate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if u.ID.String() == "" {
+				t.Error("User.ID is empty")
+			}
+			if u.CreatedAt.IsZero() {
+				t.Error("User.CreatedAt is zero")
+			}
+			if u.UpdatedAt.IsZero() {
+				t.Error("User.UpdatedAt is zero")
+			}
+		})
+	}
+}
+
+func TestUser_BeforeSave(t *testing.T) {
+	type args struct {
+		tx *gorm.DB
+	}
+
+	a := args{
+		tx: &gorm.DB{},
+	}
+
+	tests := []struct {
+		name    string
+		user    User
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "should pass",
+			user:    User{Email: "test@example.com", Password: "password"},
+			args:    a,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &User{
+				ID:         tt.user.ID,
+				Email:      tt.user.Email,
+				Password:   tt.user.Password,
+				IsVerified: tt.user.IsVerified,
+				CreatedAt:  tt.user.CreatedAt,
+				UpdatedAt:  tt.user.UpdatedAt,
+			}
+			_ = u.BeforeCreate(tt.args.tx)
+			if err := u.BeforeSave(tt.args.tx); (err != nil) != tt.wantErr {
+				t.Errorf("User.BeforeSave() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.user.Password == u.Password || u.Password == "" {
+				t.Error("User.Password is not hashed")
 			}
 		})
 	}
