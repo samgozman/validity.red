735c8acefe83e4521f26dce5e9b2f1d9bb99f94c
samgozman <sam@gozman.space>
2022-07-31 10:06:21 +0400

    Rename documentsList router

--- frontend-service/src/router/index.ts
+++ frontend-service/src/router/index.ts
@@ -37,7 +37,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (Documents.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/DocumentsView.vue"),
+      component: () => import("../views/DocumentsListView.vue"),
       meta: { requiresAuth: true },
     },
     {
