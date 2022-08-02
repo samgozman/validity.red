1ae25f0da9637bd05fecd62a1376a4a6f9ff5833
samgozman <sam@gozman.space>
2022-08-02 10:03:19 +0400

    Add notification getAll caller from frontend app

--- frontend-service/src/components/document/NotificationService.ts
+++ frontend-service/src/components/document/NotificationService.ts
@@ -1,8 +1,15 @@
-import { QueryMaker } from "@/services/QueryMaker";
+import { QueryMaker, type IResponse } from "@/services/QueryMaker";
+import type { INotification } from "./interfaces/INotification";
 
 interface NotificationDeletePayload {
   id: string;
   documentId: string;
+}
+
+interface NotificationGetAllResponse extends IResponse {
+  data: {
+    notifications: INotification[];
+  };
 }
 
 export class NotificationService {
@@ -24,5 +31,27 @@ export class NotificationService {
     if (error) {
       throw new Error(message);
     }
+  }
+
+  /**
+   * Delete notification object
+   * @returns
+   */
+  public static async getAll(documentId: string): Promise<INotification[]> {
+    const payload = JSON.stringify({
+      action: "NotificationGetAll",
+      notification: {
+        documentId,
+      },
+    });
+
+    const res = await QueryMaker.post<NotificationGetAllResponse>(payload);
+    const { error, message, data } = res.data;
+
+    if (error) {
+      throw new Error(message);
+    }
+
+    return data.notifications;
   }
 }
