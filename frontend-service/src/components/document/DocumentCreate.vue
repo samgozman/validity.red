553cd945f823958cb7810f717892ffbfd3f37f2b
samgozman <sam@gozman.space>
2022-08-07 09:48:52 +0400

    Visual modifications to create form

--- frontend-service/src/components/document/DocumentCreate.vue
+++ frontend-service/src/components/document/DocumentCreate.vue
@@ -7,13 +7,14 @@
         class="card col-span-1 row-span-1 shadow-lg lg:col-span-2 bg-base-100"
       >
         <div class="card-body form-control">
+          <h2 class="card-title">Title</h2>
           <input
             type="text"
             placeholder="Document title"
             class="input w-full"
           />
           <label class="label">
-            <span class="label-text text-base-300 px-3">
+            <span class="label-text text-base-300">
               Avoid unnecessary details.
             </span>
           </label>
@@ -21,15 +22,21 @@
       </div>
 
       <div
-        class="card col-span-1 row-span-1 shadow-lg lg:col-span-1 bg-base-100"
+        class="card col-span-1 row-span-2 shadow-lg lg:col-span-1 bg-base-100"
       >
         <div class="card-body">
-          <select class="select w-full px-0">
+          <h2 class="card-title">Document settings</h2>
+          <select class="select select-bordered w-full">
             <option disabled selected>Select document type</option>
             <option>Type 1</option>
             <option>Type 2</option>
             <option>Type 3</option>
           </select>
+          <div class="divider">Expires at</div>
+          <label class="input-group">
+            <input type="date" class="input input-bordered w-full" />
+            <span>Expiration</span>
+          </label>
         </div>
       </div>
 
@@ -37,27 +44,18 @@
         class="card col-span-1 row-span-2 shadow-lg lg:col-span-2 bg-base-100"
       >
         <div class="card-body form-control">
+          <h2 class="card-title">Description</h2>
           <textarea
             class="textarea h-full"
             placeholder="Description (optional)"
           ></textarea>
           <label class="label">
-            <span class="label-text text-base-300 px-3">
+            <span class="label-text text-base-300">
               Avoid unnecessary details. We don't need exact information about
               your document. <br />
               Please use this field only to distinguish documents from each
               other.
             </span>
-          </label>
-        </div>
-      </div>
-      <div
-        class="card col-span-1 row-span-1 shadow-lg lg:col-span-1 bg-base-100"
-      >
-        <div class="card-body form-control">
-          <label class="input-group">
-            <input type="date" class="input input-bordered w-full" />
-            <span>Expiration</span>
           </label>
         </div>
       </div>
