a8bf82efdac29f3e72330cfcd2a574e3a5ea72d7
samgozman <sam@gozman.space>
2022-08-12 09:34:28 +0400

    Add styles map

---
+++ frontend-service/src/components/document/DocumentType.ts
@@ -0,0 +1,36 @@
+interface IDocumentType {
+  name: string;
+  iconStyle: string;
+}
+
+export class DocumentType {
+  // TODO: Decode it from `document.proto::Type` if possible
+  private static types = new Map<number, IDocumentType>([
+    [0, { name: "Document", iconStyle: "" }],
+    [1, { name: "Passport", iconStyle: "" }],
+    [2, { name: "Internal passport", iconStyle: "" }],
+    [3, { name: "Foreign passport", iconStyle: "" }],
+    [4, { name: "Identity card", iconStyle: "" }],
+    [5, { name: "Driving license", iconStyle: "" }],
+    [6, { name: "Hunting license", iconStyle: "" }],
+    [7, { name: "Firearms license", iconStyle: "" }],
+    [8, { name: "Medical insurance", iconStyle: "" }],
+    [9, { name: "Property insurance", iconStyle: "" }],
+    [10, { name: "Vehicle insurance", iconStyle: "" }],
+    [11, { name: "Personal insurance", iconStyle: "" }],
+    [12, { name: "Visa", iconStyle: "" }],
+    [13, { name: "Student visa", iconStyle: "" }],
+    [14, { name: "Work permit", iconStyle: "" }],
+    [15, { name: "Residence permit", iconStyle: "" }],
+    [16, { name: "Credit card", iconStyle: "" }],
+    [17, { name: "Certificate", iconStyle: "" }],
+    [18, { name: "Vaccination Certificate", iconStyle: "" }],
+    [19, { name: "Warranty Certificate", iconStyle: "" }],
+    [20, { name: "Coupon", iconStyle: "" }],
+    [21, { name: "Other", iconStyle: "" }],
+  ]);
+
+  public static decodeType(typeId: number): IDocumentType {
+    return this.types.get(typeId) || { name: "Unknown", iconStyle: "" };
+  }
+}
