b7351f1a9b497b62cf08ddee05dbf025b30340ed
samgozman <sam@gozman.space>
2022-08-14 11:35:34 +0400

    Add notification from document view

--- frontend-service/src/components/document/NotificationsCard.vue
+++ frontend-service/src/components/document/NotificationsCard.vue
@@ -23,8 +23,41 @@ defineProps<{
           @refresh-notifications-event="refreshNotificationsEmit"
         />
       </div>
-      <div class="justify-center space-x-2 card-actions">
-        <button class="btn btn-primary btn-circle">+</button>
+      <div>
+        <form v-if="isFormActive" @submit.prevent="submit" class="mt-2">
+          <div class="form-control">
+            <div class="input-group">
+              <input
+                type="date"
+                :min="minDate"
+                class="input input-bordered w-full"
+                v-model="inputDate"
+              />
+              <input
+                type="time"
+                class="input input-bordered w-full"
+                v-model="inputTime"
+              />
+              <button class="btn btn-square btn-primary" type="submit">
+                A
+              </button>
+              <button class="btn btn-square" @click="closeFromClicked">
+                X
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div
+        v-if="isAddButtonActive"
+        class="justify-center space-x-2 card-actions"
+      >
+        <button class="btn btn-primary btn-circle" @click="addButtonClicked">
+          +
+        </button>
+      </div>
+      <div v-show="error" class="badge badge-error badge-outline w-full">
+        {{ errorMsg }}
       </div>
     </div>
   </div>
@@ -32,11 +65,56 @@ defineProps<{
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { NotificationService } from "./NotificationService";
 
 export default defineComponent({
+  data() {
+    return {
+      isFormActive: false,
+      isAddButtonActive: true,
+      minDate: new Date().toISOString().split("T")[0],
+      inputDate: "",
+      inputTime: "",
+      error: false,
+      errorMsg: "",
+    };
+  },
   methods: {
     refreshNotificationsEmit() {
       this.$emit("refreshNotificationsEvent");
+    },
+    async submit() {
+      this.error = false;
+      this.errorMsg = "";
+
+      // TODO: Is timezone correct?
+      const fullDate = new Date(`${this.inputDate}T${this.inputTime}:00`);
+      if (isNaN(fullDate.getTime())) {
+        this.error = true;
+        this.errorMsg = "Invalid date.";
+        return;
+      }
+      if (new Date(fullDate) < new Date()) {
+        this.error = true;
+        this.errorMsg = "Notification date is in the past!";
+        return false;
+      }
+
+      await NotificationService.createOne({
+        date: new Date(fullDate),
+        documentId: this.documentId,
+      });
+
+      this.closeFromClicked();
+      this.$emit("refreshNotificationsEvent");
+    },
+    addButtonClicked() {
+      this.isFormActive = true;
+      this.isAddButtonActive = false;
+    },
+    closeFromClicked() {
+      this.isFormActive = false;
+      this.isAddButtonActive = true;
     },
   },
 });
