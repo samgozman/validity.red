e6521b37335a2b1e0c3fb7bcf6835240db81b330
samgozman <sam@gozman.space>
2022-10-15 08:53:59 +0400

    Use ionicon for menu outline button

--- frontend-service/src/components/navbar/NavBar.vue
+++ frontend-service/src/components/navbar/NavBar.vue
@@ -10,19 +10,7 @@ import NavBarItems from "./NavBarItems.vue";
     <div class="navbar-start">
       <div class="flex-none md:hidden">
         <label for="left-sidebar" class="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block w-6 h-6 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
+          <ion-icon name="menu-outline" size="large"></ion-icon>
         </label>
       </div>
       <a class="btn btn-ghost normal-case text-xl"><LogoText /></a>
