68f6a01998751bdabf291546149b38d22230ab83
samgozman <sam@gozman.space>
2022-10-16 10:34:37 +0400

    Removed unused method
    After #24

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -1,25 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 type jsonResponse struct {
 	Error   bool   `json:"error"`
 	Message string `json:"message"`
 	Data    any    `json:"data,omitempty"`
-}
-
-// Decode JSON request to any struct
-func decodeJSON[T any](c *gin.Context) T {
-	var requestPayload T
-
-	if err := c.ShouldBindJSON(&requestPayload); err != nil {
-		c.AbortWithError(http.StatusBadRequest, err)
-		return requestPayload
-	}
-
-	return requestPayload
 }
