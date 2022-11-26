ceb175f1e355ff6b854a1e55fac7e2059b7bf8be
samgozman <sam@gozman.space>
2022-11-26 18:37:21 +0400

    Use default user for ssh connection between services

--- deploy/updater.sh
+++ deploy/updater.sh
@@ -5,12 +5,12 @@
 # It should be copied to the "web" server by the `web/web-config.yml`
 
 # Copy .env files (created by the github "deploy" action) to the "services" server.
-rsync -r ./calendars.env ./documents.env ./users.env updater@10.0.1.1:/validityred
+rsync -r ./calendars.env ./documents.env ./users.env root@10.0.1.1:/validityred
 # Copy .env files for "db" server
 rsync -r ./db.env updater@10.1.1.2:/validityred
 
 # Update containers on the "db" server first
-ssh updater@10.1.1.2 "
+ssh root@10.1.1.2 "
   cd /validityred
   docker compose down || true
   curl -o docker-compose.yml https://raw.githubusercontent.com/samgozman/validity.red/main/deploy/db/docker-compose.yml
@@ -19,7 +19,7 @@ ssh updater@10.1.1.2 "
 "
 
 # Update containers on "services" server
-ssh updater@10.0.1.1 "
+ssh root@10.0.1.1 "
   cd /validityred
   docker compose down || true
   curl -o docker-compose.yml https://raw.githubusercontent.com/samgozman/validity.red/main/deploy/services/docker-compose.yml
