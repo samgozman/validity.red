4fa5a22cd99ffcef858ef7a2a2cad0d69ea195fb
samgozman <sam@gozman.space>
2022-11-27 15:04:42 +0400

    Removed loader for ionicons

--- frontend-service/vite.config.ts
+++ frontend-service/vite.config.ts
@@ -6,16 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith("ion-"),
-        },
-      },
-    }),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
