0c88b37b1d744989bcfdb9d810a9539db6a3c490
samgozman <sam@gozman.space>
2022-12-08 10:41:42 +0400

    Change build target to es2020

--- frontend-service/vite.config.ts
+++ frontend-service/vite.config.ts
@@ -8,6 +8,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: "es2020",
     sourcemap: true, // Source map generation must be turned on for Sentry
   },
   plugins: [
