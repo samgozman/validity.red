fd77c0dad644ae9e18ed3140b2f6f1a7f88cad4d
samgozman <sam@gozman.space>
2022-12-09 09:49:28 +0400

    Add setup for DB backup

---
+++ deploy/services/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## Backup all databases to s3. This script is run by a cron job every day.
+## Or you can run it manually by running via github actions.
+## Provide the following secrets in github actions:
+## - B2_APPLICATION_KEY_ID
+## - B2_APPLICATION_KEY
+## They are used to authorize the b2 cli. They will be applied first time from github actions.
+
+# Create backup
+docker exec validityred-postgres-1 bash -c 'pg_dump -h localhost --port 5432 --dbname $POSTGRES_DB -U $POSTGRES_USER > /backup/validityred.sql'
+# Copy backup to BackBlaze
+export B2_APPLICATION_KEY_ID="$(<b2-app-key-id.txt)"
+export B2_APPLICATION_KEY="$(<b2-app-key.txt)"
+b2 authorize-account
+b2 sync "/backup/validityred.sql" "b2://validityred_db/validityred.sql"
+# Delete backup from container
+rm -f /backup/validityred.sql || true
