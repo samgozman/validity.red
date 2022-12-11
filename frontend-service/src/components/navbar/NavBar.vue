7cf386af73ca532bf62990540128d83dd1248b9c
samgozman <sam@gozman.space>
2022-12-11 13:19:21 +0400

    Rearrange buttons fro registered user

--- frontend-service/src/components/navbar/NavBar.vue
+++ frontend-service/src/components/navbar/NavBar.vue
@@ -6,11 +6,7 @@ import {
   PersonCircleOutline,
   LogInOutline,
 } from "@vicons/ionicons5";
-import {
-  navbarItems,
-  navbarItemsUser,
-  navbarItemsUserAuth,
-} from "@/components/navbar/items";
+import { navbarItems, navbarItemsUserAuth } from "@/components/navbar/items";
 import LogoText from "@/components/LogoText.vue";
 import NavItem from "./NavItem.vue";
 import { state } from "@/state";
@@ -51,22 +47,22 @@ import { state } from "@/state";
         <AddOutline class="w-6 lg:mr-1" />
         <span class="hidden lg:block">Add new</span>
       </RouterLink>
-      <div class="hidden md:flex dropdown dropdown-end lg:mr-4">
-        <label tabindex="0" class="btn btn-neutral btn-circle">
-          <PersonCircleOutline class="w-10" />
-        </label>
-        <ul
-          tabindex="0"
-          class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-48"
-        >
-          <NavItem
-            v-for="item in state.user.isAuthenticated
-              ? navbarItemsUserAuth
-              : navbarItemsUser"
-            v-bind:key="item.name"
-            :item="item"
-          />
-        </ul>
+      <div :class="{ hidden: !state.user.isAuthenticated }">
+        <div class="hidden md:block dropdown dropdown-end lg:mr-4">
+          <label tabindex="0" class="btn btn-neutral btn-circle">
+            <PersonCircleOutline class="w-10" />
+          </label>
+          <ul
+            tabindex="0"
+            class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-48"
+          >
+            <NavItem
+              v-for="item in navbarItemsUserAuth"
+              v-bind:key="item.name"
+              :item="item"
+            />
+          </ul>
+        </div>
       </div>
     </div>
   </div>
