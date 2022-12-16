3bfea4979a1ddbbe1f34fa274f178d3eacd92d24
samgozman <sam@gozman.space>
2022-12-16 18:45:40 +0400

    Conditional rendering for contract button

---
+++ frontend-service/src/components/elements/ContrastButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { RouteLocationRaw } from "vue-router";
+import { RouterLink } from "vue-router";
+import { defineProps } from "vue";
+
+defineProps<{
+  location: RouteLocationRaw;
+  name: string;
+}>();
+</script>
+
+<template>
+  <RouterLink
+    :to="location"
+    class="btn border-none md:px-10 bg-base-100 text-primary hover:shadow-lg hover:bg-primary hover:text-base-100 font-medium transition duration-300 ease-in-out"
+  >
+    {{ name }}
+  </RouterLink>
+</template>
