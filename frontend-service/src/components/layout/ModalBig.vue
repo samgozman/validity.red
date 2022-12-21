cd2fdfda8fa61daebeb2808a52bceefb6521dc43
samgozman <sam@gozman.space>
2022-12-21 15:24:56 +0200

    Wider modal

--- frontend-service/src/components/layout/ModalBig.vue
+++ frontend-service/src/components/layout/ModalBig.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
   <div v-bind:id="modalId" class="modal">
-    <div class="modal-box">
+    <div class="modal-box w-11/12 max-w-5xl">
       <slot />
       <div class="modal-action justify-center">
         <a href="#" class="btn btn-primary w-full">Ok 👌</a>
