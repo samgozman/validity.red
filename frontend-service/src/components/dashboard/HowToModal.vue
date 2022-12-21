b82a6c1343ee355b115601e59967a02a99d3c39d
samgozman <sam@gozman.space>
2022-12-21 16:40:08 +0200

    Add text-placeholders for macos instruction

--- frontend-service/src/components/dashboard/HowToModal.vue
+++ frontend-service/src/components/dashboard/HowToModal.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
 import ModalBig from "@/components/layout/ModalBig.vue";
 defineProps<{
   modalId: string;
@@ -8,6 +7,55 @@ defineProps<{
 
 <template>
   <ModalBig :modalId="modalId">
-    <p>Modal content</p>
+    <h3 class="font-bold text-lg">
+      How to sync notifications with your calendar
+    </h3>
+    <p class="py-4">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nihil earum
+      eos? Eligendi aut, eveniet, quisquam, eius in natus maiores cum quibusdam
+      totam pariatur nulla aperiam magni quis praesentium corrupti?
+    </p>
+    <!-- MacOs how-to -->
+    <div
+      class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box"
+    >
+      <input type="checkbox" />
+      <div class="collapse-title text-xl font-medium">
+        Sync with macOS Calendar
+      </div>
+      <div class="collapse-content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nihil
+          earum eos?
+        </p>
+        <img
+          src="/img/howto/macos_01.webp"
+          alt="MacOs calendar - add new Validity.Red subscription"
+          class="max-w-xl"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nihil
+          earum eos?
+        </p>
+        <img
+          src="/img/howto/macos_02.webp"
+          alt="MacOs calendar - edit subscription to Validity.Red"
+          class="max-w-xl"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nihil
+          earum eos?
+        </p>
+        <img
+          src="/img/howto/macos_03.webp"
+          alt="MacOs calendar - view Validity.Red notifications"
+          class="max-w-xl"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde nihil
+          earum eos?
+        </p>
+      </div>
+    </div>
   </ModalBig>
 </template>
