897a1fdd9813d2c967248674dec193e9f96c707b
samgozman <sam@gozman.space>
2023-04-23 10:59:21 +0400

    change updater script

--- deploy/updater.sh
+++ deploy/updater.sh
@@ -1,25 +1,12 @@
 #!/bin/sh
 
 # This script is used to fetch the latest version of the containers
-# on "web" server and "services" server. And then restart the containers.
-# It should be copied to the "web" server by the `web/web-config.yml`
-
-# Copy .env files (created by the github "deploy" action) to the "services" server.
-rsync -r /validityred/calendars.env /validityred/documents.env /validityred/users.env /validityred/db.env root@10.0.1.1:/validityred
-
-# Update containers on "services" server
-ssh root@10.0.1.1 "
-  cd /validityred
-  docker compose down || true
-  curl -o docker-compose.yml https://raw.githubusercontent.com/samgozman/validity.red/main/deploy/services/docker-compose.yml
-  docker compose pull
-  docker compose up --build -d
-"
+# and then restart of the containers. It should be copied to the server by the `deploy/cloud-config.yml`
 
 # Then do the same in the "web" server
-cd /validityred
+cd /validityred || exit 1
 docker compose down || true
-curl -o docker-compose.yml https://raw.githubusercontent.com/samgozman/validity.red/main/deploy/web/docker-compose.yml
+curl -o docker-compose.yml https://raw.githubusercontent.com/samgozman/validity.red/main/deploy/docker-compose.yml
 docker compose pull
 docker compose up --build -d
 
