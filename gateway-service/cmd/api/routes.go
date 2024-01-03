fab735591552641b5b7febb7189ce0cab076017d
samgozman <sam@gozman.space>
2024-01-03 13:18:22 +0200

    set allow origins

--- gateway-service/cmd/api/routes.go
+++ gateway-service/cmd/api/routes.go
@@ -18,7 +18,7 @@ func (app *Config) routes() *gin.Engine {
 
 	engine.Use(cors.New(cors.Config{
 		// TODO: Set to validity.red domains
-		AllowOrigins:     []string{"https://*", "http://*"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Authorization", "Content-Type", "sentry-trace", "baggage"},
 		AllowCredentials: true,
